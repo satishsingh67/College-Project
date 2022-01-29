2df7cf3602e0508cbc08a4cf3919b62883cc8e3a
satishsingh67 <67singh.satish@gmail.com>
2022-01-29 19:51:09 +0530

    seven commit

---
+++ src/com/College/DataBaseConnection/test - Copy.java
@@ -0,0 +1,34 @@
+package com.College.DataBaseConnection;
+
+import java.util.Date;
+
+import javax.servlet.http.Part;
+
+import com.College.DataValidation.DataValidation;
+
+public class test {
+public static void main(String[] args) {
+	/*  java.util.Date today = new java.util.Date();
+	
+	   System.out.println(new Date());
+	   System.out.println(new test().dataValidation("sss","sss@","3333","sss"));
+	   String email="aaaa";
+	   System.out.println(!(email.trim().contains("@")));
+	   
+	   */
+	Part p=null;
+	Part p1=null;
+	
+	//String result=new DataValidation().changeSecurityQuestionAnswer(" ss", " @", " ss", " s"," --Selects--", " ss", " sss");
+	
+	//System.out.println(result);
+	   
+	// dddddd  
+}
+
+String dataValidation(String fullName,String email,String mobileNumber,String message) {
+	 String result=(fullName.trim().isEmpty()?"FullName":email.trim().isEmpty()?"Email":!(email.trim().contains("@"))?"Email":mobileNumber.trim().isEmpty()?"MobileNumber":mobileNumber.trim().length()>12?"MobileNumber":message.trim().isEmpty()?"Message":"True");
+	 return result;
+}
+	
+}
