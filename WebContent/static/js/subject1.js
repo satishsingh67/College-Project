8e41662794e5edb95bbf09c0889e9a1170193133
Sornali-Hazra <sornalihazra92@gmail.com>
2022-02-25 21:15:58 +0600

    Ninth commit

---
+++ WebContent/static/js/subject1.js
@@ -0,0 +1,58 @@
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop  > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+// counter design
+ document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration /  range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 0, 20, 30);
+    counter("count2", 10, 15, 30);
+    counter("count3", 0, 14, 30);
+    counter("count4", 0, 60, 30);
+ });
+   
+   
+
+ (function ($) {
+    $("#rslider").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 1000, namespace: "callbacks",
+        before: function () {
+            $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+            $('.events').append("<li>after event fired.</li>");
+        }
+    });
+})(jQuery);
